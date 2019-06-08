201dfcf959853d4ad01374e1c8c26651772ba863
Nowroz <nowroz.blacklight@gmail.com>
2019-06-08 15:52:37 +0900

    Removed train_model funchtion

--- brand_classifier_cnn.py
+++ brand_classifier_cnn.py
@@ -23,7 +23,7 @@ all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in a
 
 def preprocess_image(image):
     image = tf.image.decode_jpeg(image, channels = 3)
-    image = tf.image.resize(image, [256, 256])
+    image = tf.image.resize(image, [32, 32])
     image /= 255.0
     return image
 
@@ -41,113 +41,100 @@ label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64
 image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
 
 
-def prepareDataset():
-    ds = image_label_ds.shuffle( buffer_size = 270)
-    ds = ds.repeat()
-    ds = ds.batch(270)
-    
-    iter = ds.make_one_shot_iterator()
-    next = iter.get_next()
-
-    X_train = next[0]
-    Y_train = next[1]
-
-    sess = tf.Session()
-
-    return sess.run(X_train), sess.run(Y_train)
-
-X_train, Y_train = prepareDataset()
-
 # Training Parameters
 learning_rate = 0.001
-num_steps = 1
+num_steps = 1000
 batch_size = 10
+image_count = 270
+
+def fetchData():
+    ds = image_label_ds.apply(
+    tf.data.experimental.shuffle_and_repeat(buffer_size=image_count))
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+
+    iter = ds.make_one_shot_iterator()
+
+    image_label_batch = iter.get_next()
+
+    image_batch = image_label_batch[0]
+    label_batch = image_label_batch[1]
+
+    return image_batch, label_batch
+
+X_train, Y_train = fetchData()
 
 # Network Parameters
-num_input = 256*256*3 # img shape: 256*256
+num_input = 32*32*3 # img shape: 32*32
 num_classes = 3 # Classes: Apple, Lenovo and Samsung
 dropout = 0.25 # Dropout, probability to drop a unit
 
 # Create the neural network
-def conv_net(x_dict, n_classes, dropout, reuse, is_training):
-    # Define a scope for reusing the variables
-    with tf.variable_scope('ConvNet', reuse=reuse):
-        # TF Estimator input is a dict, in case of multiple inputs
-        x = x_dict['images']
+# Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+x = tf.reshape(X_train, shape = [-1, 32, 32, 3])
 
-        # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
-        # Reshape to match picture format [Height x Width x Channel]
-        # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-        x = tf.reshape(x, shape=[-1, 256, 256, 3])
+# Convolution Layer with 32 filters and a kernel size of 5
+conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
+# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
-        # Convolution Layer with 32 filters and a kernel size of 5
-        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+# Convolution Layer with 64 filters and a kernel size of 3
+conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
 
-        # Convolution Layer with 64 filters and a kernel size of 3
-        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+# Flatten the data to a 1-D vector for the fully connected layer
+fc1 = tf.contrib.layers.flatten(conv2)
 
-        # Flatten the data to a 1-D vector for the fully connected layer
-        fc1 = tf.contrib.layers.flatten(conv2)
+# Fully connected layer (in tf contrib folder for now)
+fc1 = tf.layers.dense(fc1, 1024)
 
-        # Fully connected layer (in tf contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 1024)
-        # Apply Dropout (if is_training is False, dropout is not applied)
-        fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
+# Apply Dropout (if is_training is False, dropout is not applied)
+fc1 = tf.layers.dropout(fc1, rate = dropout, training = True)
 
-        # Output layer, class prediction
-        out = tf.layers.dense(fc1, n_classes)
+# Output layer, class prediction
+out = tf.layers.dense(fc1, num_classes)
 
-    return out
 
 # Define the model function (following TF Estimator Template)
-def model_fn(features, labels, mode):
-    # Build the neural network
-    # Because Dropout have different behavior at training and prediction time, we
-    # need to create 2 distinct computation graphs that still share the same weights.
-    logits_train = conv_net(features, num_classes, dropout, reuse=False,
-                            is_training=True)
-    logits_test = conv_net(features, num_classes, dropout, reuse=True,
-                           is_training=False)
 
-    # Predictions
-    pred_classes = tf.argmax(logits_test, axis=1)
-    pred_probas = tf.nn.softmax(logits_test)
+# Build the neural network
+# Because Dropout have different behavior at training and prediction time, we
+# need to create 2 distinct computation graphs that still share the same weights.
 
-    # If prediction mode, early return
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
+logits_train = out
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logits_train, labels = tf.cast(Y_train, dtype=tf.int32)))
+    
 
-        # Define loss and optimizer
-    loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits_train, labels=tf.cast(labels, dtype=tf.int32)))
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss_op,
-                                  global_step=tf.train.get_global_step())
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
 
-    # Evaluate the accuracy of the model
-    acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
+train_op = optimizer.minimize(loss_op, global_step = tf.train.get_global_step())
 
-    # TF Estimators requires to return a EstimatorSpec, that specify
-    # the different ops for training, evaluating, ...
-    estim_specs = tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions=pred_classes,
-        loss=loss_op,
-        train_op=train_op,
-        eval_metric_ops={'accuracy': acc_op})
+init = tf.global_variables_initializer()
 
-    return estim_specs
 
-# Build the Estimator
-model = tf.estimator.Estimator(model_fn)
+with tf.Session() as sess:
+    sess.run(init)
+    #print(sess.run(tf.report_uninitialized_variables()))
+    for step in range(num_steps):
+        sess.run(train_op)
+        print("Loss for step # %d: %f"%(step, sess.run(loss_op)))
 
-# Define the input function for training
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'images': X_train}, y=Y_train,
-    batch_size=batch_size, num_epochs=None, shuffle=True)
-# Train the Model
-model.train(input_fn, steps=num_steps)
+
+##logits_test = conv_net(features, num_classes, dropout, reuse=True,is_training=False)
+
+# Predictions
+##pred_classes = tf.argmax(logits_test, axis=1)
+##pred_probas = tf.nn.softmax(logits_test)
+
+# for i in range(100):
+# _ = sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+
+# accuracy = sess.run(pred_classes, feed_dict={x: X_test, y: Y_test})
+# Evaluate the accuracy of the model
+##acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
+
+
+
+
